0bd8643ed8f5ae953e847d0094d3eddffe6d4afd
goesjon <60227006+goesjon@users.noreply.github.com>
2023-01-16 20:45:08 +0100

    Add files via upload

---
+++ functions/addMusicalNoise.m
@@ -0,0 +1,111 @@
+function [sDis,fs] = addMusicalNoise(s,fs,SNR,k)
+% OSS_LICENSE
+%
+% This function adds musical noise distortions to a clean speech signal
+%
+% For further information, see:
+% Goesswein, Jonathan Albert; Kollmeier, Birger & Rennies, Jan
+% Method to control the amount of musical noise for speech quality assessments
+% JASA express letters
+%
+% This function requires the speech processing toolbox "voicebox"
+% (https://github.com/ImperialCollegeLondon/sap-voicebox)
+% and the large time frequency analysis toolbox "LTFAT"
+% (https://github.com/ltfat/ltfat)
+%
+% If you are using OCTAVE instead of MATLAB and your signal does not have
+% a sampling frequency of 16kHz, this function requires the OCTAVE
+% signal package to resample the signal
+% (https://gnu-octave.github.io/packages/signal/)
+%
+% _inputs_
+% s - input clean speech signal
+% fs - sampling frequency of input signal in Hz
+% SNR - signal-to-noise-ratio in dB
+% k - subtraction parameter >= 0
+%
+% The input parameters SNR and k control
+% the amount of musical noise distortions
+%
+% _output_
+% sDis - input speech signal with musical noise distortions
+% fs - sampling frequency of output signal
+%
+% Copyright (C) 2022 Fraunhofer IDMT, Jonathan Albert Goesswein
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+%
+% author          : Jonathan Albert Goesswein
+% email           : jonathan.goesswein@idmt.fraunhofer.de
+% date            : 16-November-2022
+% Matlab          : 9.3.0.713579 (R2017b)
+% Octave          : 7.2.0
+%
+% See also: main, spectralSubtractionAlgo
+%
+
+%% checks
+
+if size(s,2) > 1
+  s = s(:,1);
+  warning('input signal has multiple channels, channel 1 selected')
+end
+if fs ~= 16000 % Hz
+  % the function "resample" is part of the signal package for OCTAVE
+  s = resample(s,16000,fs);
+  fs = 16000; % Hz
+  warning('sampling frequency not 16kHz, signal resampled at 16kHz')
+end
+if k < 0
+   error('k is < than 0! k has to be >= 0!')
+end
+
+%% calculation
+
+IS = 0.25; % s
+
+% speech signal
+s = s(:);
+% the function 'v_activlev' is part of the
+% speech processing toolbox "voicebox"
+level_s = v_activlev(s,fs,'d');
+s = [zeros(IS*fs,1);s];
+signalLength = length(s);
+
+% noise signal
+% the function 'noise' is part of the
+% Large Time/Frequency Analysis Toolbox (LTFAT)
+w = noise(signalLength,'white');
+level_w = 10*log10(mean(w.^2));
+targetLevel_w = level_s - SNR;
+gain = targetLevel_w - level_w;
+w = w .* 10.^(gain / 20);
+
+% mixed signals
+x_plus = s + w;
+x_minus = s - w;
+
+% spectral subtraction
+a = 2; % 2 - power spectrum subtraction, 1 - magnitude spectral subtraction
+alpha = 0; % 0 - no "noise floor", otherwise small positive value
+sHat_plus = spectralSubtractionAlgo(x_plus,fs,IS,a,k,alpha);
+sHat_minus = spectralSubtractionAlgo(x_minus,fs,IS,a,k,alpha);
+sDis = sHat_plus + sHat_minus;
+sDis = sDis(IS*fs+1:end);
+
+% calibration
+level_sDis = activlev(sDis,fs,'d');
+gain = level_s - level_sDis;
+sDis = sDis .* 10.^(gain / 20);
+end
---
+++ functions/spectralSubtractionAlgo.m
@@ -0,0 +1,134 @@
+function sHat = spectralSubtractionAlgo(x,fs,IS,a,k,alpha)
+% OSS_LICENSE
+%
+% This function is a spectral subtraction noise reduction algorithm
+% 
+% Calculation according to [1] and [2]
+% This is a modified version of [3]
+% For furth information, see [4]
+%
+% _inputs_
+% x - noisy speech signal
+% fs - sampling frequency
+% IS - initial silence (noise only) in seconds
+% a - Magnitude Power 
+% (a = 1 for magnitude spectral subtraction, 
+%  a = 2 for power spectrum subtraction)
+% k - controls the amount of subtraction
+% alpha - 0 or some small positive value to create a "noise floor"
+%
+% _output_
+% sHat - estimated speech
+% contains clean speech, residual noise and musical noise artifacts
+%
+% [1] Boll, S. (1979)
+% Suppression of acoustic noise in speech using spectral subtration
+% IEEE Transactions on Acoustics, Speech, and Signal Processing, 27(2), 113-120
+% https://doi.org/10.1109/TASSP.1979.1163209
+%
+% [2] Thiemann, J. (2001)
+% Acoustic Noise Supression for Speech Signals using Auditory Masking Effects [Master Thesis]
+% McGill University, Montreal, Canada
+% https://escholarship.mcgill.ca/concern/theses/vm40xt64p
+%
+% [3] Zavarehei, Esfandiar (2022)
+% Multi-band Spectral Subtraction
+% https://www.mathworks.com/matlabcentral/fileexchange/7674-multi-band-spectral-subtraction
+% MATLAB Central File Exchange. Retrieved November 15, 2022.
+%
+% [4] Goesswein, Jonathan Albert; Birger & Rennies, Jan
+% Method to control the amount of musical noise for speech quality assessments
+%
+% Copyright (C) 2022 Fraunhofer IDMT, Jonathan Albert Goesswein
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+%
+% author          : Jonathan Albert Goesswein          
+% email           : jonathan.goesswein@idmt.fraunhofer.de                  
+% date            : 16-November-2022           
+% Matlab          : 9.3.0.713579 (R2017b) 
+% Octave          : 7.2.0
+%
+% See also: main, spectralSubtractionAlgo
+%
+
+%%
+
+windowLength = fix(0.025*fs);
+shiftPerc = 0.4;
+window = hamming(windowLength);
+
+numSilSegs = fix((IS*fs-windowLength)/(shiftPerc*windowLength) +1);
+
+xSeg = segment(x,windowLength,shiftPerc,window);
+Xseg = fft(xSeg,windowLength);
+phiXseg = angle(Xseg(1:fix(end/2)+1,:));
+absXseg = abs(Xseg(1:fix(end/2)+1,:)).^a;
+numFrames = size(absXseg,2);
+
+absWhatSeg = mean(absXseg(:,1:numSilSegs),2);
+
+Tmp2 = zeros(windowLength/2+1,numFrames); % init
+for indFrame = 1:numFrames
+  Tmp = absXseg(:,indFrame)-(k*absWhatSeg);
+  Tmp2(:,indFrame) = max(Tmp,alpha);
+end
+
+sHat = overlapAdd(Tmp2.^(1/a),phiXseg,windowLength,shiftPerc*windowLength);
+
+function signal = overlapAdd(y,yPhase,windowLength,shiftLength)
+
+if fix(shiftLength) ~= shiftLength
+    shiftLength = fix(shiftLength);
+end
+
+[~,sizeY2] = size(y);
+
+Spectrum = y.*exp(1i*yPhase);
+
+if mod(windowLength,2)
+    Spectrum = [Spectrum ; flipud(conj(Spectrum(2:end,:)))];
+else
+    Spectrum = [Spectrum ; flipud(conj(Spectrum(2:end-1,:)))];
+end
+
+signal = zeros((sizeY2-1)*shiftLength+windowLength,1);
+
+for ind = 1:sizeY2
+    start = (ind-1) * shiftLength + 1;
+    spectrum = Spectrum(:,ind);
+    signal(start:start+windowLength-1) = signal(start:start+windowLength-1)+real(ifft(spectrum,windowLength));
+end
+
+end
+
+function Segment = segment(signal,windowLength,shiftPerc,Window)
+
+Window = Window(:);
+
+lengthSignal = length(signal);
+shiftPerc = fix(windowLength.*shiftPerc);
+numberOfSegments = fix((lengthSignal-windowLength)/shiftPerc +1);
+
+Index = (repmat(...
+  1:windowLength,...
+  numberOfSegments,1)+repmat((0:(numberOfSegments-1))'*shiftPerc,...
+  1,...
+  windowLength))';
+
+repWindow = repmat(Window,1,numberOfSegments);
+Segment = signal(Index) .* repWindow;
+end
+end
+
